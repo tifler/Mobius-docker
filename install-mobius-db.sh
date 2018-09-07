715e58e2c031c7e8281308027da28566efbe2553
Youngdo, Lee <ydlee@anapass.com>
2018-09-07 17:16:53 +0900

    Add MOBIUS_DB_PORT to indicate db's port number

--- install-mobius-db.sh
+++ install-mobius-db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "Step 1 --"
-echo "create database mobiusdb;" | mysql -u root --password=${MOBIUS_DB_PASS} --host=${MOBIUS_DB_HOST}
-echo "Step 2 --"
-cat ${MOBIUS_BASE}/mobius/mobiusdb.sql | mysql -u root --password=${MOBIUS_DB_PASS} --host=${MOBIUS_DB_HOST} --database=mobiusdb
-echo "Step 3 --"
+echo "Step 1/3 --"
+echo "create database mobiusdb;" | mysql -u root --password=${MOBIUS_DB_PASS} --host=${MOBIUS_DB_HOST} --port=${MOBIUS_DB_PORT}
+echo "Step 2/3 --"
+cat ${MOBIUS_BASE}/mobius/mobiusdb.sql | mysql -u root --password=${MOBIUS_DB_PASS} --host=${MOBIUS_DB_HOST} --port=${MOBIUS_DB_PORT} --database=mobiusdb
+echo "Step 3/3 --"
